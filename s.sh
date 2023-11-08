f996740da2d72b5bb4d42aafa09c61c4ba741a64
bhargavi <bhargavi01@gmail.com>
2023-11-08 11:29:21 +0000

    Pushing new file

---
+++ s.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+javac file1.java
+java file1.java
+
+javac file2.java
+java file2.java
+
+javac file3.java
+java file3.java
+
+javac file4.java
+java file4.java
+
+javac file5.java
+java file5.java
